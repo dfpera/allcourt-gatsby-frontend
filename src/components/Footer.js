0a1ec306fafaa2f1aac2c562001d77e600c24a1f
Daniele Perazzolo <dfpera@gmail.com>
2022-02-26 20:32:18 -0800

    Update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const Footer = () => {
-
   return (
-    <footer></footer>
+    <footer className='
+      pt-10
+      pb-3
+      -mb-[3.5rem]
+      text-right
+    '>
+      &copy; {new Date().getFullYear()} Dubu Studios Ltd.
+    </footer>
   )
 }
 
